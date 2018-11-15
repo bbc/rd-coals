77723a8870941f9f393d6260bfaee4fdd4032902
Michael sparks <michael.sparks@bbc.co.uk>
2018-11-15 13:22:42 +0000

    remove directories so they can be replaced with submodules

--- packages/ecs_core/README.txt
+++
@@ -1,1 +0,0 @@
-A simple ECS Core, with a focus on JSON Serialisation
--- packages/ecs_core/ecs_core/__init__.py
+++
@@ -1,7 +0,0 @@
-#
-# Copyright 2018 British Broadcasting Corporation
-#
-# Author: Michael Sparks <michael.sparks@bbc.co.uk>
-#
-# All Rights Reserved
-#
